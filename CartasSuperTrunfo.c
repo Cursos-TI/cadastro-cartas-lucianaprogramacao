79b31eb7fc3b5dfc3179dc1908c5a77462d7ee43
lucianaprogramacao <lucianaacj2017@gmail.com>
2026-08-17 21:10:17 +0000

    Organizei na ordem correta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,12 +2,15 @@
 
 int main() {
 
-    
+    // =========================
+    // CARTA 1
+    // =========================
+
     printf("\n--- CARTA 1 ---\n");
+
     char estado1;
     char codigo1[4];
     char cidade1[50];
-
     int populacao1;
     float area1;
     float pib1;
@@ -36,13 +39,21 @@ int main() {
     scanf("%d", &pontos_turisticos1);
 
 
-printf("Estado: %c\n", estado1);
-printf("Codigo: %s\n", codigo1);
-printf("Nome da Cidade: %s", cidade1);
-printf("Populacao: %d\n", populacao1);
-printf("Area: %.2f km2\n", area1);
-printf("PIB: %.2f bilhoes de reais\n", pib1);
-printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
+    // Exibindo Carta 1
+
+    printf("\n--- DADOS DA CARTA 1 ---\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km2\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
+
+
+    // =========================
+    // CARTA 2
+    // =========================
 
     printf("\n--- CARTA 2 ---\n");
 
@@ -54,37 +65,40 @@ printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
     float pib2;
     int pontos_turisticos2;
 
+    printf("Digite o estado da Carta 2: ");
+    scanf(" %c", &estado2);
 
-printf("Digite o Estado da carta 2: ");
-scanf(" %c", &estado2);
+    printf("Digite o codigo da Carta 2: ");
+    scanf("%s", codigo2);
 
-printf("Digite o codigo da carta 2: ");
-scanf(" %s", codigo2);
+    printf("Digite o nome da cidade da Carta 2: ");
+    getchar();
+    fgets(cidade2, sizeof(cidade2), stdin);
 
-printf("Digite o Nome da Cidade da carta 2: ");
-scanf("%S", cidade2);
+    printf("Digite a populacao da Carta 2: ");
+    scanf("%d", &populacao2);
 
-printf("Digite a Populacao da carta 2: ");
-scanf("%d", &populacao2);
+    printf("Digite a area da Carta 2: ");
+    scanf("%f", &area2);
 
-printf("Digite a Area da carta 2: ");
-scanf("%f", &area2);
+    printf("Digite o PIB da Carta 2: ");
+    scanf("%f", &pib2);
 
-printf("Digite o PIB da carta 2: ");
-scanf("%f", &pib2);
-
-printf("Digite os Numeros de pontos turisticos da carta 2: ");
-scanf("%d", &pontos_turisticos2);
-
-
-printf("Estado: %c\n", estado2);
-printf("Codigo: %s\n", codigo2);
-printf("Nome da Cidade: %s\n", cidade2);
-printf("Populacao: %d\n", populacao2);
-printf("Area: %.2f km2\n", area2);
-printf("PIB: %.2f bilhoes de reais\n", pib2);
-printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos2); 
+    printf("Digite o numero de pontos turisticos da Carta 2: ");
+    scanf("%d", &pontos_turisticos2);
 
 
-return 0;
+    // Exibindo Carta 2
+
+    printf("\n--- DADOS DA CARTA 2 ---\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km2\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos2);
+
+
+    return 0;
 }
